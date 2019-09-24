04c9b8fd322704b68230fb2fa6097ff0c375db4d
Jeremie <admin@ubisofts-mini.ubisoft.org>
2019-09-24 15:56:34 +0200

    with context api

--- src/App.js
+++ src/App.js
@@ -1,71 +1,42 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext, createContext } from "react";
 
-const buttonReducer = (state, action) => {
-  switch(action.type) {
-    case 'push':
-      return {
-        ...state,
-        isPushed: !state.isPushed
-      }
-    default:
-      return state;
-  }
-}
-
-const useReducer = (reducer, initialState) => {
-  const [ state, setState ] = useState(initialState);
-
-  function dispatch(action) {
-    const nextState = reducer(state, action);
-    setState(nextState);
-  }
-
-  return [state, dispatch] ;
-}
+const ButtonContext = createContext({
+  isPushed: false,
+  setPushed: () => {}
+})
 
 const Button = () => {
-  const [buttonState, dispatch] = useReducer(buttonReducer, {
-    isPushed: false
-  });
-
-  const buttonRef = useRef(null);
-
-  const testFn = text => {
-    const { current } = buttonRef;
-    current.innerText = `${text} wesh alors`;
-  };
-
-  useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      testFn(i);
-      i += 1;
-      if (i > 5) {
-        clearInterval(interval);
-        dispatch({
-          type: 'push',
-          payload: {
-            isPushed: true
-          }
-        })
-      }
-    }, 1000);
-  }, []);
-
-  const { isPushed } = buttonState;
   return (
-    <button ref={buttonRef} onClick={() => testFn('bruh')}>
+    <ButtonContext.Consumer>
+    {({isPushed, setPushed}) => <button onClick={setPushed}>
       {isPushed ? "true" : "false"}
     </button>
+    }
+    </ButtonContext.Consumer>
   );
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.setPushed = () => {
+      this.setState(state => ({
+        isPushed: !state.isPushed
+      }))
+    }
+
+  this.state = {
+    isPushed: false,
+    setPushed: this.setPushed 
+  };
 }
 
-export default App;
+  render() {
+    return (
+      <ButtonContext.Provider value={this.state}>
+        <Button />
+      </ButtonContext.Provider>
+    );
+  }
+}
