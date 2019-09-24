7721de7e2926349442e929351cb18840be0ed738
Jeremie <admin@ubisofts-mini.ubisoft.org>
2019-09-24 15:55:19 +0200

    useState + useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+
+const buttonReducer = (state, action) => {
+  switch(action.type) {
+    case 'push':
+      return {
+        ...state,
+        isPushed: !state.isPushed
+      }
+    default:
+      return state;
+  }
+}
+
+const useReducer = (reducer, initialState) => {
+  const [ state, setState ] = useState(initialState);
+
+  function dispatch(action) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  }
+
+  return [state, dispatch] ;
+}
+
+const Button = () => {
+  const [buttonState, dispatch] = useReducer(buttonReducer, {
+    isPushed: false
+  });
+
+  const buttonRef = useRef(null);
+
+  const testFn = text => {
+    const { current } = buttonRef;
+    current.innerText = `${text} wesh alors`;
+  };
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      testFn(i);
+      i += 1;
+      if (i > 5) {
+        clearInterval(interval);
+        dispatch({
+          type: 'push',
+          payload: {
+            isPushed: true
+          }
+        })
+      }
+    }, 1000);
+  }, []);
+
+  const { isPushed } = buttonState;
+  return (
+    <button ref={buttonRef} onClick={() => testFn('bruh')}>
+      {isPushed ? "true" : "false"}
+    </button>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button />
     </div>
   );
 }
