4eba0c0b60fc03855cdd0541ce42d4fa56992df7
Jeremie <admin@ubisofts-mini.ubisoft.org>
2019-09-24 15:57:13 +0200

    with useContext

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React, { useState, useRef, useEffect, useContext, createContext } from "react";
+import React, { useState, useContext, createContext } from "react";
 
-const ButtonContext = createContext({
-  isPushed: false,
-  setPushed: () => {}
-})
+const ButtonContext = createContext([{}, () => {}])
 
-const Button = () => {
-  return (
-    <ButtonContext.Consumer>
-    {({isPushed, setPushed}) => <button onClick={setPushed}>
-      {isPushed ? "true" : "false"}
-    </button>
-    }
-    </ButtonContext.Consumer>
-  );
-};
+const App = () => {
+  const [isPushed, setPushed] = useState(false);
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-
-    this.setPushed = () => {
-      this.setState(state => ({
-        isPushed: !state.isPushed
-      }))
-    }
-
-  this.state = {
-    isPushed: false,
-    setPushed: this.setPushed 
-  };
-}
-
-  render() {
+  const Button = () => {
+    const [isPushed, setPushed] = useContext(ButtonContext)
     return (
-      <ButtonContext.Provider value={this.state}>
-        <Button />
-      </ButtonContext.Provider>
+      <button onClick={setPushed}>
+        {isPushed ? "true" : "false"}
+      </button>
     );
-  }
+  };
+
+  return (
+    <ButtonContext.Provider value={[isPushed, () => setPushed(!isPushed)]}>
+      <Button />
+    </ButtonContext.Provider>
+  );
 }
+
+export default App;
